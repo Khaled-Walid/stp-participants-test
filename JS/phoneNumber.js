0c589b6ec6d948af4bc986600b95b907ba770a64
Khaled Walid <justkhwf@outlook.com>
2020-10-14 19:08:06 +0200

    hint

--- JS/phoneNumber.js
+++ JS/phoneNumber.js
@@ -1,8 +1,12 @@
 const phoneNumber = document.getElementById("Phone");
+// const hint = document.getElementById("hint");
 
 const pattern = /^01[0-25]\d{8}$/gs;
 
+hint.style.display = "none"
+
 function validatePhoneNumber() {
+  // hint.style.removeProperty("display")
   if (!pattern.test(phoneNumber.value)) {
     phoneNumber.style.backgroundColor = "#dc35462c";
   } else { 
