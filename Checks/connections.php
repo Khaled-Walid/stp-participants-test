fd19216131f4cdd4c969bb81f15216ff33df1cd8
Khaled Walid <justkhwf@outlook.com>
2020-10-18 16:54:21 +0200

    delete old merge

--- Checks/connections.php
+++
@@ -1,6 +0,0 @@
-<?php
-    $dpserver = "localhost";
-    $dpusername = "root";
-    $dppassword = "";
-    $dpname ="recruitment";
-    $conn = mysqli_connect($dpserver,$dpusername,$dppassword,$dpname); 
